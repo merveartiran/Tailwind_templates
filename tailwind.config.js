60272b099a356f11b887a9aaacdead2f0d08eb4d
Merve Artıran <martiran@etu.edu.tr>
2024-12-18 13:08:29 +0100

    margin added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container:{
+      center: true,
+      padding: '16px',
+
+
+    },
+    extend: {
+      colors: {
+        'primary': '#c996a5',
+      'secondary': '#f6e3ba',
+      'tertiary': '#dcaf92',
+      'background': '#93a59a',
+      'fortis': '#e86060',
+      },
+      fontFamily:{
+        
+          miuul: ['Happy Monkey', 'system-ui'],
+          open:['Open Sans', 'sans-serif']
+          
+        
+        
+      },
+    },
   },
   plugins: [],
 }
